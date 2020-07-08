9a9c9e573ad7002c49659f80f8ce32450975f3c6
Younho Choo <younho9.choo@gmail.com>
2020-07-08 21:08:56 +0900

    feat: 백엔드 기본 구조 추가 (#26)
    * feat: 데이터베이스 구현  #19 (#22)
    
    * feat: package.json 추가
    
    프로젝트를 관리하고 npm module을 설치하기 위한 package.json 파일 설정
    
    * chore: gitignore에 backend/node_modules 추가
    
    용량이 큰 node_modules는 git으로 관리하지 않고 package.json으로 관리하며 npm install 으로 설치
    
    * chore: package-lock.json 생성
    
    npm install로 modules 설치하고 생성된 package-lock.json 파일
    
    * feat: 서버와 라우터 기본 폼 작성
    
    node server.js로실행 후 localhose:3000 으로 접속하면 회원가입 폼이 나오는 백엔드 서버
    
    * feat: 회원 정보를 저장하기 위한 데이터베이스 생성
    
    post 요청이 왔을 때 NeDB 를 사용하여 회원 정보를 저장하는 메소드 작성
    
    * fix: router 변수 스테이지에 안올린 것 추가
    
    main.js 라우터 변수 커밋에 추가했습니다.
    
    * refactor: 서버와 라우터 기본 구조 작성 (#24)
    
    * feat: package.json 추가
    
    프로젝트를 관리하고 npm module을 설치하기 위한 package.json 파일 설정
    
    * feat: 서버와 라우터 기본 폼 작성
    
    node server.js로실행 후 localhose:3000 으로 접속하면 회원가입 폼이 나오는 백엔드 서버
    
    * refactor: 폴더 구조 변경
    
    backend, frontend로 나누었던 폴더 구조 변경
    
    Co-authored-by: LimHaksu <limhaksu@naver.com>
    
    * chore: .gitignore 수정
    
    .gitignore에 backend/node_modules node_modules로 수정
    
    Co-authored-by: LimHaksu <32071079+LimHaksu@users.noreply.github.com>
    Co-authored-by: LimHaksu <limhaksu@naver.com>

---
+++ router/main.js
@@ -0,0 +1,30 @@
+module.exports = function (app, fs, db) {
+  app.get('/', function (req, res) {
+    res.render('sign-up.html');
+  });
+
+  app.get('/sign-up', function (req, res) {
+    res.render('sign-up.html');
+  });
+
+  app.post('/add-user', function (req, res) {
+    const user = {
+      id: req.body['id'],
+      password: req.body['password'],
+      passwordCheck: req.body['passwordCheck'],
+      emailId: req.body['emailId'],
+      emailSite: req.body['emailSite'],
+      name: req.body['name'],
+      phone: req.body['phone'],
+      zipCode: req.body['zipCode'],
+      address: req.body['address'],
+      addressDetail: req.body['addressDetail'],
+      isOptionalTermChecked: req.body['optionalTermChecked'],
+    };
+    db.insert(user, function (err) {
+      const result = { success: 1 };
+      res.json(result);
+      console.log('저장 완료');
+    });
+  });
+};
