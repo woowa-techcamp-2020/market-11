6eeccac2f96cf2ff518262b0294dcf7ed02f2428
younho9 <younho9.choo@gmail.com>
2020-07-08 23:36:56 +0900

    feat: 회원가입 마크업 pug 템플릿으로 변환

--- router/router.js
+++ router/router.js
@@ -2,11 +2,11 @@ const utils = require('./utils');
 
 module.exports = function (app, db) {
   app.get('/sign-up', function (req, res) {
-    res.render('sign-up.html');
+    res.render('sign-up.pug');
   });
 
   app.get('/sign-up-complete', function (req, res) {
-    res.render('sign-up-complete.html');
+    res.render('sign-up-complete.pug');
   });
 
   app.post('/add-user', function (req, res) {
