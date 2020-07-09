7c43c840708acf0e96f4bc93f7529e4632fd5eb7
LimHaksu <limhaksu@naver.com>
2020-07-09 15:24:53 +0900

    feat: 에러 체크를 위한 자바스크립트 validation파일 생성
    input과 error의 id를 이용하여 DOM 객체 가져오기

---
+++ public/js/validation.js
@@ -0,0 +1,14 @@
+const inputId = document.getElementById('id');
+const inputPassword = document.getElementById('password');
+const inputPasswordCheck = document.getElementById('password-check');
+const inputEmailId = document.getElementById('email-id');
+const inputEmailSite = document.getElementById('email-site');
+const inputName = document.getElementById('name');
+const inputPhone = document.getElementById('phone');
+
+const errorId = document.getElementById('error-id');
+const errorPassword = document.getElementById('error-password');
+const errorPasswordCheck = document.getElementById('error-password-check');
+const errorEmail = document.getElementById('error-email');
+const errorName = document.getElementById('error-name');
+const errorPhone = document.getElementById('error-phone');
