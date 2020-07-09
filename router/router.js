4915df92603678bf3074bd276207fa5b0881c60c
Younho Choo <younho9.choo@gmail.com>
2020-07-09 09:31:18 +0900

    feat: html 마크업 pug 템플릿으로 변환 (#31)
    * refactor: 경로 변경
    
    * feat: main.js를 router로 변경 및 utils 모듈화
    
    * feat: 회원가입 마크업 pug 템플릿으로 변환
    
    * feat: 회원가입 완료 페이지 pug 템플릿 변환
    
    * refactor: app.engine() 파라미터 수정
    
    app.engine() 의 파라미터를 document에 나온 방식으로 수정
    
    Co-authored-by: LimHaksu <limhaksu@naver.com>

---
+++ router/router.js
@@ -0,0 +1,15 @@
+const utils = require('./utils');
+
+module.exports = function (app, db) {
+  app.get('/sign-up', function (req, res) {
+    res.render('sign-up.pug');
+  });
+
+  app.get('/sign-up-complete', function (req, res) {
+    res.render('sign-up-complete.pug');
+  });
+
+  app.post('/add-user', function (req, res) {
+    utils.createUser(req, res);
+  });
+};
--- server.js
+++ server.js
@@ -9,18 +9,18 @@ const Database = require('nedb');
 //자동 로드 및 데이터베이스 파일 생성
 const db = new Database({ filename: 'user.db', autoload: true });
 
-app.set('views', __dirname + '/../frontend/views'); // 서버가 읽을 수 있도록 html의 위치를 정의해줌.
-app.set('view engine', 'ejs'); // ejs는 템플릿 엔진, pug도 템플릿 엔진, 서버가 html을 렌더링 할때 ejs엔진을 사용하도록 설정
-app.engine('html', require('ejs').renderFile);
+app.set('views', __dirname + '/views'); // 서버가 읽을 수 있도록 html의 위치를 정의해줌.
+app.set('view engine', 'pug'); // ejs는 템플릿 엔진, pug도 템플릿 엔진, 서버가 html을 렌더링 할때 ejs엔진을 사용하도록 설정
+app.engine('pug', require('pug').__express);
 
 const server = app.listen(3000, function () {
   console.log('Express server has started on port 3000');
 });
 
-app.use(express.static('../frontend/public')); // 정적파일의 루트 지정 : html에서 사용되는 .js 파일, css 파일, image파일 등
+app.use(express.static('public')); // 정적파일의 루트 지정 : html에서 사용되는 .js 파일, css 파일, image파일 등
 // 서버에서 정적파일을 다룰땐 express.static() 메소드를 사용하면 된다.
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
-const router = require('./router/main')(app, fs, db); // main.js 에서 export한 함수에 app을 파라미터로 넣어줌.
+const router = require('./router/router')(app, db); // main.js 에서 export한 함수에 app을 파라미터로 넣어줌.
 // 라우터 모듈인 main.js를 불러와서 app에 전달해줌
