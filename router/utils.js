e43bfbf5c9154498407807455f1e64a677d46711
LimHaksu <32071079+LimHaksu@users.noreply.github.com>
2020-07-09 13:27:54 +0900

    feat: 회원가입 기능 구현 #16 (#32)
    * fix: module.exports 잘못 입력한것 수정
    
    exports.module --> module.exports 로 수정
    
    * fix: utils.createUser() 메소드에 db 파라미터 추가
    
    기존에 db를 전달하지 않아서 db를 찾을 수 없다는 에러가 발생하는 문제 해결
    
    * feat: 패스워드를 암호화하는 함수 생성
    
    평문 패스워드를 입력하면 암호화된 패스워드와
    암호화할때 사용한 솔트 값을 리턴하는 함수 제작
    
    * refactor: DB에 저장하는 회원정보 중 불필요한 속성 제거
    
    passwordCheck는 프론트엔드에서 일치하는지 확인하는 용도기때문에 데이터베이스에 저장할 필요가 없음
    
    * feat: 암호화된 패스워드를 DB에 저장
    
    암호화된 패스워드와 암호화할때 사용한 솔트 값을 데이터베이스에 저장
    
    * chore: getEncryptedPasswordAndSalt() 함수에 대한 주석 수정
    
    리턴 타입을 JSON에서 Promise로 수정
    
    * feat: 아이디 중복체크하는 함수 생성
    
    아이디가 데이터베이스에 이미 존재하면 true, 없으면 false 반환하는 함수 제작
    
    * feat: 아이디 중복 체크하는 로직 추가
    
    회원 가입할때 createUser() 메소드에서  아이디 중복 체크하는 로직을 추가

--- router/utils.js
+++ router/utils.js
@@ -1,9 +1,16 @@
-exports.modules = {
-  createUser: function (req, res) {
+module.exports = {
+  createUser: async function (req, res, db) {
+    const userId = req.body['id'];
+    const idDuplication = await isIdDuplicated(db, userId);
+    if(idDuplication){
+      res.json({success: 0});
+      console.log('아이디 중복');
+      return;
+    }
+    const encryptedPasswordAndSalt = await getEncryptedPasswordAndSalt(req.body['password']);
     const user = {
-      id: req.body['id'],
-      password: req.body['password'],
-      passwordCheck: req.body['passwordCheck'],
+      id: userId,
+      password: encryptedPasswordAndSalt.encryptedPassword,
       emailId: req.body['emailId'],
       emailSite: req.body['emailSite'],
       name: req.body['name'],
@@ -12,6 +19,7 @@ exports.modules = {
       address: req.body['address'],
       addressDetail: req.body['addressDetail'],
       isOptionalTermChecked: req.body['optionalTermChecked'],
+      salt: encryptedPasswordAndSalt.salt,
     };
     db.insert(user, function (err) {
       const result = { success: 1 };
@@ -20,3 +28,37 @@ exports.modules = {
     });
   },
 };
+
+/**
+ * 평문 비밀번호를 암호화하고 암호화된 비밀번호와 솔트 값을 리턴
+ * @param {string} password 
+ * @returns {Promise<{encryptedPassword : string, salt : string}>} promise
+ */
+function getEncryptedPasswordAndSalt(password){
+  const crypto = require('crypto');
+  return new Promise((resolve, reject)=>{
+    crypto.randomBytes(64, (err, buf) => { // 64바이트 랜덤 salt 생성
+      crypto.pbkdf2(password, buf.toString('base64'), 100000, 64, 'sha512', (err, key) => { // base64방식으로 표현, 10만번 반복, sha512 알고리즘 사용,
+        resolve({encryptedPassword : key.toString('base64'), salt : buf.toString('base64')});
+      });
+    });
+  });
+}
+
+/**
+ * 아이디 중복을 확인
+ * @param {Nedb} db
+ * @param {string} id
+ * @returns {Promise<boolean>} 중복된 아이디면 true, 아니면 false 반환 
+ */
+function isIdDuplicated(db, id){
+  return new Promise((resolve, reject)=>{
+    db.find({"id":id}, function(err, docs){
+      if(docs.length > 0){
+        resolve(true);
+      }else{
+        resolve(false);
+      }
+    });
+  });
+}
