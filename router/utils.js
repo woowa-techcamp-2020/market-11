912307590d1a494580c3e9becb5b407666fc7f79
LimHaksu <limhaksu@naver.com>
2020-07-09 11:20:03 +0900

    feat: 패스워드를 암호화하는 함수 생성
    평문 패스워드를 입력하면 암호화된 패스워드와
    암호화할때 사용한 솔트 값을 리턴하는 함수 제작

--- router/utils.js
+++ router/utils.js
@@ -20,3 +20,19 @@ module.exports = {
     });
   },
 };
+
+/**
+ * 평문 비밀번호를 암호화하고 암호화된 비밀번호와 솔트 값을 리턴
+ * @param {string} password 
+ * @returns {{string, string}} {encryptedPassword, salt}
+ */
+function getEncryptedPasswordAndSalt(password){
+  const crypto = require('crypto');
+  return new Promise((resolve, reject)=>{
+    crypto.randomBytes(64, (err, buf) => { // 64바이트 랜덤 salt 생성
+      crypto.pbkdf2(password, buf.toString('base64'), 100000, 64, 'sha512', (err, key) => { // base64방식으로 표현, 10만번 반복, sha512 알고리즘 사용,
+        resolve({encryptedPassword : key.toString('base64'), salt : buf.toString('base64')});
+      });
+    });
+  });
+}
