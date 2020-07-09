9ccfcef32dc38266d3f47715484f04f003044734
younho9 <younho9.choo@gmail.com>
2020-07-09 10:55:44 +0900

    feat: 선택 정보 입력 토글 기능 구현

--- public/js/index.js
+++ public/js/index.js
@@ -18,3 +18,27 @@ const updateCheckBox = () => {
 allTerms.addEventListener('click', checkAll);
 essentialTerms.addEventListener('click', updateCheckBox);
 optionTerms.addEventListener('click', updateCheckBox);
+
+const optionInfo = document.getElementById('option-info');
+const optionInfoCheck = document.getElementById('option-info-check');
+
+const toggleAddress = (e) => {
+  let status = e.target.checked ? 'disable' : 'enable';
+  let optionElements = optionInfo.getElementsByClassName(status);
+
+  if (e.target.checked) {
+    Array.from(optionElements).forEach((el) => {
+      el.classList.remove('disable');
+      el.classList.add('enable');
+      el.removeAttribute('disabled');
+    });
+  } else {
+    Array.from(optionElements).forEach((el) => {
+      el.classList.remove('enable');
+      el.classList.add('disable');
+      el.setAttribute('disabled', '');
+    });
+  }
+};
+
+optionInfoCheck.addEventListener('click', toggleAddress);
