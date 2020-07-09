212dbf3949d622da39bc2470f03780f652e01b5e
LimHaksu <limhaksu@naver.com>
2020-07-09 15:28:01 +0900

    feat: 회원 아이디 유효성 체크하는 함수 제작, input에 이벤트 연결

--- public/js/validation.js
+++ public/js/validation.js
@@ -13,6 +13,11 @@ const errorEmail = document.getElementById('error-email');
 const errorName = document.getElementById('error-name');
 const errorPhone = document.getElementById('error-phone');
 
+inputId.addEventListener('blur', function(event){
+    const id = event.target.value;
+    const message = validateId(id);
+    handleErrorMessage(errorId, message);
+});
 /**
  * 에러 메세지가 있을 경우 에러 영역에 메세지 추가하고 hidden 속성 제거
  * 에러 메세지가 없을 경우 hidden 속성 추가하여 에러메세지 가림
@@ -27,3 +32,20 @@ function handleErrorMessage(errorLabel, message){
         errorLabel.classList.add('hidden');
     }
 }
+
+/**
+ * @param {string} id 
+ * @returns {string} 유효한 ID면 빈 문자열, 아닌 경우 에러 문자열 반환
+ */
+function validateId(id){
+    if(id.length === 0){
+        return '아이디를 입력해주세요.';
+    }
+    const regExp = /^[a-zA-Z0-9_-]{4,20}$/g;
+    if(regExp.test(id)){
+        return '';
+    }else{
+        return '아이디는 영문과 숫자로 4자~20자 사이로 입력해 주세요.';
+    }
+}
+
