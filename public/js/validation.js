4cd8ecd8c23739ac88e4760685676bfd03ec33e0
LimHaksu <limhaksu@naver.com>
2020-07-09 15:26:28 +0900

    feat: 에러메세지를 p태그에 넣는 함수 제작

--- public/js/validation.js
+++ public/js/validation.js
@@ -12,3 +12,18 @@ const errorPasswordCheck = document.getElementById('error-password-check');
 const errorEmail = document.getElementById('error-email');
 const errorName = document.getElementById('error-name');
 const errorPhone = document.getElementById('error-phone');
+
+/**
+ * 에러 메세지가 있을 경우 에러 영역에 메세지 추가하고 hidden 속성 제거
+ * 에러 메세지가 없을 경우 hidden 속성 추가하여 에러메세지 가림
+ * @param {DOM} errorLabel
+ * @param {string} id 
+ */
+function handleErrorMessage(errorLabel, message){
+    if(message !== ''){
+        errorLabel.innerText = message;
+        errorLabel.className = errorLabel.className.replace(/\bhidden\b/g, ''); // Cross-Browser 고려한 remove class name
+    }else{
+        errorLabel.classList.add('hidden');
+    }
+}
